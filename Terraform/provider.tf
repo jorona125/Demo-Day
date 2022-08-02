248be79590a23f904b8bf8a848a9350439b63c21
Jose Orona <josemorona@outlook.com>
2022-08-02 11:02:25 -0400

    adding userdata to instance and variable files

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,8 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.23.0"
-    }
-  }
+provider "aws"{
+        access_key = "${var.AWS_ACCESS_KEY}"
+        secret_key = "${var.AWS_SECRET_KEY}"
+        region     = "us-east-1"
 }
