f88c265b0990e5be3f1e7aace92683217e59884a
jorona125 <104389403+jorona125@users.noreply.github.com>
2023-01-09 21:31:29 -0500

    Update Testsfinstall.sh

--- Testsfinstall.sh
+++ Testsfinstall.sh
@@ -1,25 +1,23 @@
-    cat << EOF > /etc/yum.repos.d/vscode.repo
-    [code]
-    name=Visual Studio Code
-    baseurl=https://packages.microsoft.com/yumrepos/vscode
-    enabled=1
-    gpgcheck=1
-    gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-    EOF
+#!/usr/bin/env bash
 
-    yum install -y code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat << EOF >  /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+yum install -y code
+###install python###
+sudo yum update
+sudo yum install python3
 
-    ###install python###
-    sudo yum update
-    sudo yum install python3
-
-    ##install docker###
-    sudo yum install -y yum-utils
-    sudo yum-config-manager \
-        --add-repo\
-        https://download.docker.com/linux/rhel/docker/docker-ce.repo
-    sudo yum install docker-ce docker-ce-cli container.io docker-compose-plugin
-
-    ###install aws CLI###
-    yum install awscli -y 
-    
+##install docker###
+sudo yum check-update
+sudo yum install -y yum-utils device-mapper-persisten-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce -y
+   ###install aws CLI###
+yum install awscli -y
